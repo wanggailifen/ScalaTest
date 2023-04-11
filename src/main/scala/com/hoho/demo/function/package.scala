7a63db0187b2839db97e2410833f41f8c3c158e6
tonglc <1502694581@qq.com>
2023-04-11 12:18:00 +0800

    函数

---
+++ src/main/scala/com/hoho/demo/function/package.scala
@@ -0,0 +1,7 @@
+package com.hoho.demo
+
+package object function {
+  def testPackageObject(): Unit = {
+    println("haha 我是包对象的方法")
+  }
+}
---
+++ src/main/scala/com/hoho/demo/oop/Test01_Package.scala
@@ -0,0 +1,28 @@
+package com {
+  package hoho {
+    package demo {
+      package hoho {
+
+        object Test01_Package {
+          def main(args: Array[String]): Unit = {
+            /**
+             * TODO 可以将包也当成对象
+             *  ** 包对象中声明的属性和方法，在当前包和它的子包都可以直接访问
+             *  ** scala中包名和物理路径没有关系
+             * */
+
+            oop.testPackageObject()
+
+          }
+        }
+      }
+
+    }
+
+  }
+
+}
+
+
+
+
