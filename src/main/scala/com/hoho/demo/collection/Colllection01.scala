2c3cb411ce8b68158fc959e8a8d1e50ac5226327
离分 <tonglicheng.tlc@alibaba-inc.com>
2023-04-18 11:11:16 +0800

    array

--- src/main/scala/com/hoho/demo/collection/Colllection01.scala
+++ src/main/scala/com/hoho/demo/collection/Colllection01.scala
@@ -1,10 +1,16 @@
 package com.hoho.demo.collection
 
 object Colllection01 {
+
+
+
   def main(args: Array[String]): Unit = {
     // Scala 集合主要也分为三大类：
     // 1. Seq：存儲有序，数据可重复
     // 2. Set：存储无序，数据不可重复
     // 3. Map ：存储无序，存儲KV健值对，KEY是不能重复的，VALUE是可以重复的。
   }
+
+
+
 }
